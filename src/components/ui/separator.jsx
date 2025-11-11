0e798e37534188b7eff41b63fa8ae9c5348b96e7
Dell Property Management <info@dellpropertymanagement.com>
2025-11-11 01:09:28 -0600

    Contact form: hard-wire Formspree endpoint xrbarqgz

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,1 +1,20 @@
-﻿import * as React from "react"import * as SeparatorPrimitive from "@radix-ui/react-separator"import { cn } from "../../lib/utils"const Separator = React.forwardRef((  { className, orientation = "horizontal", decorative = true, ...props },  ref) => (  <SeparatorPrimitive.Root    ref={ref}    decorative={decorative}    orientation={orientation}    className={cn(      "shrink-0 bg-border",      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",      className    )}    {...props} />))Separator.displayName = SeparatorPrimitive.Root.displayNameexport { Separator }
+﻿import * as React from "react";
+import { cn } from "../../lib/utils";
+
+function Separator({ className, orientation = "horizontal", decorative = true, ...props }) {
+  return (
+    <div
+      role={decorative ? "none" : "separator"}
+      aria-orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Separator };
+export default Separator;
